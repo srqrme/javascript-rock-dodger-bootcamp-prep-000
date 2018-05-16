78ec075b439c58fc962150dd4e368dc9c09d982a
Sharmaine Requerme <srqrme@gmail.com>
2018-05-16 16:41:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  
+
   clearInterval(gameInterval)
   GAME.removeChild(ROCKS[0])
   ROCKS.shift()
