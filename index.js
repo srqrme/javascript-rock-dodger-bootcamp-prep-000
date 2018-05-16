38f9942f459a80e6a62c61c22cb8dc9c4188c305
Sharmaine Requerme <srqrme@gmail.com>
2018-05-16 16:38:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ function createRock(x) {
 function endGame() {
   while(ROCKS.length > 0) {
     GAME.removeChild(ROCKS[0])
-    ROCKS.shift
+    ROCKS.shift()
   }
 
   clearInterval(gameInterval)
